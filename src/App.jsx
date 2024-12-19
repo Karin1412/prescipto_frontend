d065a2e97a7cd428c966eb57026b22221812b5b2
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-12-20 02:29:50 +0700

    set route

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Header from './components/layout/Header';
 import Contact_Page from './components/Contact_Page';
 import My_Appointments_Page from './components/My_Appointments_Page';
 import Home_Page from './components/Home_Page';
+import Layout from './layout';
 
 const App = () => {
     return (
         <div>
-            <Header/>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home_Page />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/contact" element={<Contact_Page />} />
-                <Route path="/me/my-appointments" element={<My_Appointments_Page />} />
-            </Routes>
-        </Router>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home_Page />} />
+                        <Route path="/contact" element={<Contact_Page />} />
+                        <Route path="/me/my-appointments" element={<My_Appointments_Page />} />
+                    </Route>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
+            
         </div>
     );
 };
