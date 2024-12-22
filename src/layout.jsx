daff1d7b1062645602ec6b490c8d2dbb680d5032
Pauline <quynh190504@gmail.com>
2024-12-22 14:23:50 +0700

    Add footer

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 
 const Layout = () => {
@@ -15,6 +16,7 @@ const Layout = () => {
       <main className="min-h-screen">
         <Outlet />
       </main>
+        {!noHeaderFooter && <Footer />}
     </>
   );
 };
