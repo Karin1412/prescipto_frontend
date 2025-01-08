c35dd0dc6bce2330a1d555c29c8180479578922b
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2025-01-08 14:01:28 +0700

    acceptdata

---
+++ src/data/goodsReceiptNotesData.jsx
@@ -0,0 +1,64 @@
+const goodsReceiptNotesData = [
+  {
+    id: "PN00001",
+    supplierId: "NCC003",
+    importDate: "2024-01-05",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00002",
+    supplierId: "NCC002",
+    importDate: "2024-01-01",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00003",
+    supplierId: "NCC001",
+    importDate: "2024-01-04",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00004",
+    supplierId: "NCC003",
+    importDate: "2024-01-24",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00005",
+    supplierId: "NCC001",
+    importDate: "2024-01-24",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00006",
+    supplierId: "NCC001",
+    importDate: "2024-01-19",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00007",
+    supplierId: "NCC004",
+    importDate: "2024-01-30",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00008",
+    supplierId: "NCC005",
+    importDate: "2024-01-11",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00009",
+    supplierId: "NCC001",
+    importDate: "2024-01-06",
+    totalCost: 200000000
+  },
+  {
+    id: "PN00010",
+    supplierId: "NCC002",
+    importDate: "2024-01-20",
+    totalCost: 200000000
+  },
+];
+
+export default goodsReceiptNotesData;
---
+++ src/data/medicinesData.jsx
@@ -0,0 +1,75 @@
+const medicinesData = [
+    {
+      id: "TC001",
+      medicineName: "Paracetamol",
+      importDate: "2024-01-15",
+      expiryDate: "2025-01-15",
+      supplierId: "NCC001",
+      unit: "viên",
+      usesage: "Giảm đau, hạ sốt",
+      dosage: "500mg mỗi 4-6 giờ, không quá 4g mỗi ngày",
+      status: "Đang sử dụng",
+      quantity: 100,
+      importPrice: 100000000,
+      sellingPrice: 200000000
+    },
+    {
+      id: "TC002",
+      medicineName: "Amoxicillin",
+      importDate: "2024-02-10",
+      expiryDate: "2025-02-10",
+      supplierId: "NCC002",
+      unit: "viên",
+      usesage: "Kháng sinh điều trị nhiễm khuẩn",
+      dosage: "500mg mỗi 8 giờ",
+      status: "Đang sử dụng",
+      quantity: 200,
+      importPrice: 200000000,
+      sellingPrice: 200000000
+    },
+    {
+      id: "TC003",
+      medicineName: "Ibuprofen",
+      importDate: "2024-03-20",
+      expiryDate: "2025-03-20",
+      supplierId: "NCC003",
+      unit: "viên",
+      usesage: "Giảm đau, kháng viêm",
+      dosage: "200-400mg mỗi 4-6 giờ",
+      status: "Đang sử dụng",
+      quantity: 150,
+      importPrice: 300000000,
+      sellingPrice: 200000000
+    },
+    {
+      id: "TC004",
+      medicineName: "Cefuroxime",
+      importDate: "2024-04-15",
+      expiryDate: "2025-04-15",
+      supplierId: "NCC004",
+      unit: "viên",
+      usesage: "Kháng sinh điều trị nhiễm trùng",
+      dosage: "250-500mg mỗi 12 giờ",
+      status: "Ngừng sử dụng",
+      quantity: 50,
+      importPrice: 400000000,
+      sellingPrice: 200000000
+    },
+    {
+      id: "TC005",
+      medicineName: "Aspirin",
+      importDate: "2024-05-05",
+      expiryDate: "2025-05-05",
+      supplierId: "NCC005",
+      unit: "viên",
+      usesage: "Giảm đau, hạ sốt, chống đông máu",
+      dosage: "75-325mg mỗi ngày",
+      status: "Đang sử dụng",
+      quantity: 120,
+      importPrice: 500000000,
+      sellingPrice: 200000000
+    }
+  ];
+  
+  export default medicinesData;
+  
